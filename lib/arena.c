4179ac8ed42e1c931625f8e353dd3a6d9f80f2aa
mainak55512 <mbhattacharjee432@gmail.com>
2025-12-13 00:42:47 +0530

    added memory alignment logic

--- lib/arena.c
+++ lib/arena.c
@@ -8,32 +8,128 @@ typedef struct Arena {
 	struct Arena *next;
 } Arena;
 
+void *m_align_alloc(size_t capacity) {
+	size_t alignment;
+	size_t header_size;
+	size_t total_size;
+	void *origin;
+	char *current;
+	size_t current_addr;
+	size_t mask;
+	size_t aligned_addr;
+	char *aligned_ptr;
+	char **head;
+
+	alignment = 8;
+	header_size = sizeof(void *);
+
+	/*
+	 * (alignment - 1) ensures proper padding is done and no byte is wasted
+	 * for most cases sizeof(void *) is 8 bytes, so for worst case scenario we
+	 * need maximum 7 bytes for padding.
+	 * */
+	total_size = capacity + (alignment - 1) + header_size;
+
+	origin = malloc(total_size);
+	if (origin == NULL) {
+		return NULL;
+	}
+
+	/*
+	 * This moves the pointer to starting of memory block of ((alignment - 1) +
+	 * aligned memory)
+	 * */
+	current = (char *)origin + header_size;
+
+	/*
+	 * current and (alignment - 1) needs to be casted to an integer type
+	 * as we need to do bit manipulation later
+	 * */
+	current_addr = (size_t)current;
+	mask = (size_t)(alignment - 1);
+
+	aligned_addr = (current_addr + mask) & ~mask;
+
+	/*
+	 * casting to char* for pointer arithmetic
+	 * */
+	aligned_ptr = (char *)aligned_addr;
+
+	head = (char **)(aligned_ptr - header_size);
+	*head = (char *)origin;
+
+	return (void *)aligned_ptr;
+}
+
+void m_align_free(void *ptr) {
+	char **head;
+	void *origin;
+
+	if (ptr == NULL) {
+		return;
+	}
+
+	/*
+	 * head is a pointer-to-pointer which stores the address of the original
+	 * address returned by malloc. ptr is the aligned pointer, so if we move
+	 * back by a size of a pointer we will have the address where the original
+	 * pointer is stored.
+	 *
+	 *     v-head
+	 * [   |========][============]
+	 * ^-malloc      ^-aligned memory
+	 * */
+	head = (char **)((char *)ptr - sizeof(void *));
+	origin = (void *)*head;
+
+	free(origin);
+}
+
 Arena *arena_init(size_t capacity) {
-	Arena *arena = (Arena *)malloc(sizeof(Arena));
-	void *data = malloc(capacity);
+	Arena *arena;
+	void *data;
+
+	arena = (Arena *)m_align_alloc(sizeof(Arena));
+	data = m_align_alloc(capacity);
+
 	if (arena == NULL || data == NULL) {
 		fprintf(stderr, "Fatal: Failed to allocate arena");
 		abort();
 	}
+
 	arena->region = data;
 	arena->capacity = capacity;
 	arena->size = 0;
 	arena->next = NULL;
+
 	return arena;
 }
 
 void *arena_alloc(Arena *arena, size_t size) {
-	Arena *current_arena = arena;
-	void *data = NULL;
+	Arena *current_arena;
+	void *data;
+	Arena *new_arena;
+
+	current_arena = arena;
+	data = NULL;
+
+	/*
+	 * if the element fits in the arena, then store it.
+	 * */
 	if (current_arena->size + size <= current_arena->capacity) {
 		data = (char *)current_arena->region + current_arena->size;
 		current_arena->size += size;
-	} else {
+	}
+	/*
+	 * if it doesn't fit, check if it has any other arena attached to it.
+	 * if no arena is attached, then allocate a new one.
+	 * */
+	else {
 		if (current_arena->next != NULL) {
 			current_arena = current_arena->next;
 			data = arena_alloc(current_arena, size);
 		} else {
-			Arena *new_arena = NULL;
+			new_arena = NULL;
 			if (current_arena->capacity < size) {
 				new_arena = arena_init(size);
 			} else {
@@ -43,6 +139,7 @@ void *arena_alloc(Arena *arena, size_t size) {
 			data = arena_alloc(new_arena, size);
 		}
 	}
+
 	return data;
 }
 
@@ -53,9 +150,9 @@ void arena_free(Arena **arena) {
 	if ((*arena)->next != NULL) {
 		arena_free(&(*arena)->next);
 	}
-	free((*arena)->region);
+	m_align_free((*arena)->region);
 	(*arena)->region = NULL;
-	free(*arena);
+	m_align_free(*arena);
 	*arena = NULL;
 	return;
 }
